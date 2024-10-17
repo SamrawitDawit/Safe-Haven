375c24b96302679977ea617aca8563ee5cfaaa5b
SamrawitDawit <samrikdawit@gmail.com>
2024-10-17 12:14:58 +0300

    Fixed google sign in for web

---
+++ frontend/src/app/auth/dummy-dashboard/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import React from "react";
+
+
+const SignInSuccess = () => {
+  return (
+    <div className="flex justify-center">
+      <h1>You have successfully signed in with Google!</h1>
+    </div>
+  );
+};
+
+export default SignInSuccess;
